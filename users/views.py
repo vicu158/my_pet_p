cdcc376e787930b94bfeab61888dea2741ccd7f5
vicu158 <v_evlashev@mail.ru>
2024-07-25 00:31:59 +0300

    4.11.2

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.contrib import auth
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -31,3 +31,15 @@ def registration(request):
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+        context = {'title': 'Store- Профиль', 'form': form}
+        return render(request, 'users/profile.html', context)
