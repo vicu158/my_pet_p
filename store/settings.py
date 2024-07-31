600ae9fca584b3ea55fa13d0c1ef7b6e33a321d8
vicu158 <v_evlashev@mail.ru>
2024-07-31 20:16:49 +0300

    7.9

--- store/settings.py
+++ store/settings.py
@@ -94,3 +94,6 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
