11923890986a6c22c2956a82ffabd27012efe835
vicu158 <v_evlashev@mail.ru>
2024-08-02 17:39:31 +0300

    7.12

--- store/settings.py
+++ store/settings.py
@@ -98,4 +98,10 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'storewelcomestore@gmail.com'
+EMAIL_HOST_PASSWORD = 'zxrlaaztenfyigbu'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
--- users/models.py
+++ users/models.py
@@ -11,6 +11,7 @@ from django.core.mail import send_mail
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', null=True, blank=True)
     is_verified_email = models.BooleanField(default=False)
+    email = models.EmailField(unique=True)
 
 
 class EmailVerification(models.Model):
@@ -33,7 +34,8 @@ class EmailVerification(models.Model):
         send_mail(
             subject=subject,
             message=message,
-            from_email='from@example.com',
+            # from_email='from@example.com',
+            from_email=settings.EMAIL_HOST_USER,
             recipient_list=[self.user.email],
             fail_silently=False
         )
