dc01eadcec25bb862f18711031800ef19f690179
vicu158 <v_evlashev@mail.ru>
2024-08-03 17:01:31 +0300

    9.3

---
+++ products/tests.py
@@ -0,0 +1,17 @@
+from http import HTTPStatus
+
+from django.test import TestCase
+from django.urls import reverse
+
+from products.models import Product, ProductCategory
+
+
+class IndexViewTestCase(TestCase):
+
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store')
+        self.assertTemplateUsed(response, 'products/index.html')
