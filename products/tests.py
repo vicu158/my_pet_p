b6c6587d9f26181b01e22814484a691e4dd2230a
vicu158 <v_evlashev@mail.ru>
2024-08-03 17:45:30 +0300

    9.5

--- products/tests.py
+++ products/tests.py
@@ -3,8 +3,6 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
-from products.models import Product, ProductCategory
-
 
 class IndexViewTestCase(TestCase):
 
